ef88ea63d8edcb36ce2e2ba0b1125e7df6844259
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2017-12-14 15:34:40 -0800

    Initial Commit

---
+++ checksum/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  checksum
+//
+//  Created by Sidhant Gandhi on 12/14/17.
+//  Copyright © 2017 newnoetic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
